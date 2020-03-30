9ed67d08648d4e15e5e4d2aec13c594938b0abaf
ramazan <pekeramazan@gmail.com>
2020-03-31 02:06:55 +0300

    finish

--- app/src/main/java/com/ramazanpeker/escapecorona/PlayGameScreen.kt
+++ app/src/main/java/com/ramazanpeker/escapecorona/PlayGameScreen.kt
@@ -2,20 +2,29 @@ package com.ramazanpeker.escapecorona
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_play_game_screen.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class PlayGameScreen : AppCompatActivity() {
     var score=0
     var ImageArray=ArrayList<ImageView>()
+    var handler= Handler()
+    var runnable=Runnable{}
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_game_screen)
 
         val intent=intent
-        val time=intent.getIntExtra("mod",30)
+        val  takeTime=intent.getIntExtra("mod",30)
+        val time=takeTime.toLong()
 
         ImageArray.add(imageView1)
         ImageArray.add(imageView2)
@@ -37,6 +46,36 @@ class PlayGameScreen : AppCompatActivity() {
         ImageArray.add(imageView18)
         ImageArray.add(imageView19)
         ImageArray.add(imageView20)
+        hideandShowandom()
+
+    object :CountDownTimer(time*1000+500,1000){
+        override fun onFinish() {
+            timeTXT.text="0"
+            handler.removeCallbacks(runnable)
+            for(image in ImageArray){
+                image.visibility=View.INVISIBLE
+            }
+            val alert=AlertDialog.Builder(this@PlayGameScreen)
+            alert.setTitle(("Oyun Bitti"))
+            alert.setMessage("Tekrar Oynamak İster misin ?")
+            alert.setPositiveButton("yes"){dialog, which ->
+                finish()
+                startActivity(intent)
+            }
+            alert.setNegativeButton("NO"){dialog, which ->
+                finish()
+
+            }
+            alert.show()
+
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+        timeTXT.text=""+millisUntilFinished/1000
+        }
+
+    }.start()
+
 
     }
 
@@ -44,6 +83,32 @@ class PlayGameScreen : AppCompatActivity() {
 
    fun increaseScore(view:View)
   {
+      score=score+1;
+      scoreText.text=""+score
 
   }
+    fun decreaseScore(view:View)
+    {
+        score=score-2;
+        scoreText.text=""+score
+
+    }
+
+
+    fun hideandShowandom(){
+        runnable=object:Runnable{
+            override fun run() {
+                for(image in ImageArray){
+                    image.visibility=View.INVISIBLE
+                }
+                val random= Random()
+                val randomIndex=random.nextInt(20)
+                ImageArray[randomIndex].visibility=View.VISIBLE
+                handler.postDelayed(runnable,700)
+            }
+
+        }
+        handler.post(runnable)
+
+    }
 }
