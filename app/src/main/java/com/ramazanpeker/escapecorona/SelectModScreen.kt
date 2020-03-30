89de0baa27537c69b4ced43e92c590703a8b8221
ramazan <pekeramazan@gmail.com>
2020-03-30 22:36:19 +0300

    mainscreeen finish

---
+++ app/src/main/java/com/ramazanpeker/escapecorona/SelectModScreen.kt
@@ -0,0 +1,18 @@
+package com.ramazanpeker.escapecorona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+
+class SelectModScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        setContentView(R.layout.activity_select_mod_screen)
+
+    }
+}
